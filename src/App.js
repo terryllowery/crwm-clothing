ad01f268cfa07f24cda35494d6b78f8868b28155
Terry Lowery <tl@lowerytech.net>
2020-09-17 12:51:22 -0500

    broke components out - styled items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-    Hello from react!
     <HomePage />
     </div>
   );
