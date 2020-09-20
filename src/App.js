fbf3ddfc3d9a1892c0feec638c8eb8badba294a1
Terry Lowery <tl@lowerytech.net>
2020-09-20 08:58:05 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
+const JacketsPage = (props) => (
+  <div>
+    <h1>Jackets Page: {props}</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div>
-    <HomePage />
-    </div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop/jackets' component={JacketsPage} />
+    
+      </Switch>
   );
 }
 
